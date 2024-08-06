5193649e36b290e27fe21af14e5556f914fba662
kouyq <549737899@qq.com>
2024-08-06 18:23:00 +0800

    tolua passed

--- src/extension.ts
+++ src/extension.ts
@@ -12,7 +12,23 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('"luaconverter" is now active!');
 
 	const disposable1 = vscode.commands.registerCommand('luaconverter.tolua', () => {
-		vscode.window.showInformationMessage('luaconverter.tolua!');
+		const editors = vscode.window.visibleTextEditors;
+		if (editors.length === 0) {
+			console.log('当前没有可见的文本编辑器');
+		} else if (editors.length === 1) {
+			const content = editors[0].document.getText();
+			const result = format(JSON.parse(content));
+			// 并列新建一个临时文档，并将result写入其中
+			vscode.workspace.openTextDocument({
+				language: 'lua',
+				content: result
+			}).then(doc => {
+				vscode.window.showTextDocument(doc);
+				vscode.window.showInformationMessage('转换完成!');
+			});
+		} else {
+			console.log('当前有多个可见的文本编辑器，建议用户关闭并列的其它Tab页面');
+		}
 	});
 
 	const disposable2 = vscode.commands.registerCommand('luaconverter.tojson', () => {
